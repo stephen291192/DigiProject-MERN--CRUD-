369dc831138d405291cecac026a6fba7d9b77ab7
rajesh <rajesh@kpost.com>
2024-02-19 10:26:34 +0530

    front end Modification

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 // import "@coreui/coreui/dist/css/coreui.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Details from "../src/Details";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div className="App">
       {/* <p>Testing</p> */}
       <Details />
+      <ToastContainer />
     </div>
   );
 }
--- FrontEnd/src/Details.js
+++ FrontEnd/src/Details.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   CForm,
   CFormInput,
@@ -11,7 +11,13 @@ import { CSmartTable } from "@coreui/react-pro";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { Button } from "@coreui/coreui";
+import Select from "react-select";
+import moment from "react-moment";
+import axios from "axios";
+import { toast } from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css';
 
+// import { ToastContainer,toast } from 'react-toast'
 function Details() {
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
@@ -23,6 +29,21 @@ function Details() {
   const [phone, setPhone] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [qualification, setqualification] = useState("");
+  const [qualificationError, setqualificationError] = useState("");
+  const [fnameError, setFnameError] = useState("");
+  const [FanameError, setFanameError] = useState("");
+  const [mnameError, setMnameError] = useState("");
+  const [mailError, setMailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+
+  const [getData, setGetData] = useState([]);
+
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   const clearFuntion = () => {
     setFname("");
@@ -33,71 +54,93 @@ function Details() {
     setFaname("");
     setMname("");
     setDate("");
-    setGender("");
+    setqualification("");
+    setFnameError("");
+    setFanameError("");
+    setMnameError("");
+    setMailError("");
+    setPhoneError("");
+    // setLnameError("")
   };
-
-  const createFuntion = () => {
-    alert("waiting");
-  };
-
-  const data = [
-    {
-      name: "Rajesh",
-      Lname: "V",
-      age: "15",
-      email: "dsfsd@gmail.com",
-    },
-    {
-      name: "Rajesh",
-      Lname: "V",
-      age: "15",
-      email: "dsfsd@gmail.com",
-    },
-  ];
 
   const columns = [
+    {
+      key: "S_no",
+      label: "S.No",
+      _style: { width: "5%", background: "#002663", color: "#fff" },
+      _props: { color: "#fff" },
+      filter: false,
+      sorter: false,
+    },
+
+    {
+      key: "FName",
+      label: "Name",
+      _style: { width: "15%", background: "#002663", color: "#fff" },
+      _props: { color: "#fff" },
+      filter: true,
+      sorter: true,
+    },
     // {
-    //   key: "S_no",
-    //   label: "S.No",
-    //   _style: { width: "5%", background: "#002663" },
+    //   key: "LName",
+    //   label:"Last Name",
+    //   _style: { width: "5%", background: "#002663", color: "#fff" },
     //   _props: { color: "#fff", className: "fw-semibold" },
-    //   filter: false,
-    //   sorter: false,
+    //   filter: true,
+    //   sorter: true,
     // },
-
+    // {
+    //   key: "FatherName",
+    //   label:"Father Name",
+    //   _style: { width: "10%", background: "#002663", color: "#fff" },
+    //   _props: { color: "#fff", className: "fw-semibold" },
+    //   filter: true,
+    //   sorter: true,
+    // },
+    // {
+    //   key: "MotherName",
+    //   label: "Parent Name",
+    //   _style: { width: "15%", background: "#002663", color: "#fff" },
+    //   _props: { color: "#fff" },
+    //   filter: true,
+    //   sorter: true,
+    // },
     {
-      key: "name",
+      key: "DOB",
+      label: "DOB",
       _style: { width: "15%", background: "#002663", color: "#fff" },
       _props: { color: "#fff", className: "fw-semibold" },
       filter: true,
       sorter: true,
     },
     {
-      key: "Lname",
+      key: "qualification",
+      label: "Qualification",
       _style: { width: "15%", background: "#002663", color: "#fff" },
       _props: { color: "#fff", className: "fw-semibold" },
       filter: true,
       sorter: true,
     },
     {
-      key: "email",
+      key: "Email",
+      label: "Email",
       _style: { width: "15%", background: "#002663", color: "#fff" },
       _props: { color: "#fff", className: "fw-semibold" },
       filter: true,
       sorter: true,
     },
     {
-      key: "age",
+      key: "PhoneNumber",
+      label: " Number",
       _style: { width: "15%", background: "#002663", color: "#fff" },
       _props: { color: "#fff", className: "fw-semibold" },
       filter: true,
       sorter: true,
     },
-
     {
       key: "show_details",
       label: "Action",
-      _style: { width: "6%", background: "#002663", color: "#fff" },
+      _style: { width: "12%", background: "#002663", color: "#fff" },
       filter: false,
       sorter: false,
       _props: { color: "#fff", className: "fw-semibold" },
@@ -106,18 +149,198 @@ function Details() {
 
   // Validation
 
-  // const FNameValidation={()=>{
+  const FNameValidation = (e) => {
+    const firstName = e.target.value.trim();
 
-  // }}
+    if (firstName.length < 3) {
+      setFnameError("First name must be at least 3 characters long");
+    } else if (/\d/.test(firstName)) {
+      // Check if the first name contains any numbers using a regular expression
+      setFnameError("First name cannot contain numbers");
+    } else {
+      setFnameError("");
+    }
+  };
+
+  const FatherNameValidation = (e) => {
+    const FatherName = e.target.value.trim();
+
+    if (FatherName.length < 3) {
+      setFanameError("Father name must be at least 3 characters long");
+    } else if (/\d/.test(FatherName)) {
+      setFanameError("Father name cannot contain numbers");
+    } else {
+      setFanameError("");
+    }
+  };
+
+  const MotherNameValidation = (e) => {
+    const motherName = e.target.value.trim();
+
+    if (motherName.length < 3) {
+      setMnameError("Mother name must be at least 3 characters long");
+    } else if (/\d/.test(motherName)) {
+      setMnameError("Mother name cannot contain numbers");
+    } else {
+      setMnameError("");
+    }
+  };
+
+  const QualificationValidation = (e) => {
+    const qualification = e.target.value.trim();
+
+    if (qualification.length < 2) {
+      setqualificationError(
+        "Qualification name must be at least 2 characters long"
+      );
+      // } else if (/\d/.test(qualification)) {
+      //   setqualificationError("Qualification name cannot contain numbers");
+    } else {
+      setqualificationError("");
+    }
+  };
+
+  const emailValidation = (e) => {
+    const email = e.target.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setMailError("Invalid email format");
+    } else {
+      setMailError("");
+    }
+  };
+
+  const PhoneNumberValidation = (e) => {
+    const phoneNumber = e.target.value.trim();
+    const phoneRegex = /^\d{10}$/;
+
+    if (!phoneRegex.test(phoneNumber)) {
+      setPhoneError(
+        "Invalid phone number format. Maximum 10 digits only allowed."
+      );
+    } else {
+      setPhoneError("");
+    }
+  };
+
+  //  Validation END
+
+  // API Function Start
+
+  useEffect(() => {
+    GetDetails();
+  }, []);
+  //   Get Task API
+  const GetDetails = async () => {
+    setGetData([]);
+    try {
+      const response = await axios.get(`http://localhost:3001/api/GetDetails`);
+      const data = response.data.detailsvalue.map((x, i) => {
+        return {
+          DetailsID: x._id,
+          S_no: i + 1,
+          FName: x.fname,
+          LName: x.lname,
+          FatherName: x.Faname,
+          MotherName: x.mname,
+          DOB: new Date(x.date).toLocaleDateString(),
+          Gender: x.gender,
+          Email: x.mail,
+          PhoneNumber: x.phone,
+          qualification: x.qualification,
+        };
+      });
+
+      setGetData(data);
+      console.log("responseresponse", data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const SaveDetails = async () => {
+    const data = {
+      fname,
+      lname,
+      Faname,
+      mname,
+      date,
+      mail,
+      phone,
+      qualification,
+    };
+
+    try {
+      const response = await axios.post(`http://localhost:3001/api/save`, data);
+
+      if (response.status === 200 || response.status === 201) {
+        console.log("response", response);
+        toast.success("Details Created Successfully");
+        GetDetails();
+        clearFuntion();
+      } else {
+        toast.error("An error occurred");
+      }
+    } catch (error) {
+      toast.error("An error occurred ", error);
+    }
+  };
+
+  //update API Function
+
+  const DetailsUpdate = async (detailsID) => {
+    const data = {
+      fname,
+      lname,
+      Faname,
+      mname,
+      date,
+      mail,
+      phone,
+      qualification,
+    };
+
+    try {
+      const response = await axios.post(
+        `http://localhost:3001/api/detailsUpdate/${detailsID}`,
+        data
+      );
+
+      if (response.status === 200) {
+        return await response.json();
+      } else if (response.status === 401) {
+        return await response.json();
+      } else if (response.status === 400 || response.status === 404) {
+        return await response.json();
+      } else {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.error);
+      }
+    } catch (err) {
+      console.error("Error updating details:", err);
+    }
+  };
+
+  // Delete API Function
+  const DeleteTask = async (detailsID) => {
+    try {
+      await axios.delete(
+        `http://localhost:3001/api/detailsdelete/${detailsID}`
+      );
+      toast.success(" Details Delete Successfully");
+      GetDetails();
+    } catch (error) {
+      console.error(`Error deleting task with ID ${detailsID}:`, error);
+    }
+  };
 
   return (
-    <div className="">
+    <div className="m-3">
       <CRow className="">
         <CCol sm={4}>
-          <CForm className=" m-5">
+          <CForm className="">
             <CRow>
               <CCol sm={8}>
-                {" "}
                 <CFormLabel className="mt-2">
                   First Name <code>*</code>
                 </CFormLabel>
@@ -126,9 +349,11 @@ function Details() {
                   value={fname}
                   onChange={(e) => {
                     setFname(e.target.value);
+                    FNameValidation(e);
                   }}
                   placeholder="First Name "
                 />
+                <span className="errorMsg">{fnameError}</span>
               </CCol>
               <CCol sm={4}>
                 {" "}
@@ -140,7 +365,7 @@ function Details() {
                   onChange={(e) => {
                     setLname(e.target.value);
                   }}
-                  disabled={!fname}
+                  disabled={!fname || fnameError}
                   type="lName"
                   placeholder="Last Name"
                 />
@@ -155,12 +380,15 @@ function Details() {
                   value={Faname}
                   onChange={(e) => {
                     setFaname(e.target.value);
+                    FatherNameValidation(e);
                   }}
-                  disabled={!fname || !lname}
-                  type="lName"
+                  disabled={!fname || !lname || fnameError}
+                  type="FName"
                   placeholder="Father Name"
                 />
+                <span className="errorMsg">{FanameError}</span>
               </CCol>
+
               <CCol sm={6}>
                 <CFormLabel className=" mt-2">
                   Mother Name <code>*</code>
@@ -169,11 +397,15 @@ function Details() {
                   value={mname}
                   onChange={(e) => {
                     setMname(e.target.value);
+                    MotherNameValidation(e);
                   }}
-                  disabled={!fname || !Faname || !lname}
+                  disabled={
+                    !fname || !Faname || !lname || FanameError || fnameError
+                  }
                   type="lName"
                   placeholder="Mother Name"
                 />
+                <span className="errorMsg">{mnameError}</span>
               </CCol>
             </CRow>
             <CRow>
@@ -187,64 +419,103 @@ function Details() {
                   onChange={(e) => {
                     setDate(e.target.value);
                   }}
-                  disabled={!fname || !Faname || !mname || !lname}
-                  type=""
-                  placeholder="1-100"
+                  disabled={
+                    !fname ||
+                    !Faname ||
+                    !mname ||
+                    !lname ||
+                    FanameError ||
+                    mnameError ||
+                    fnameError
+                  }
+                  type="date"
+                  placeholder="dd/mm/yyyy"
                 />
               </CCol>
               <CCol sm={6}>
-                {" "}
-                <CFormLabel className=" mt-2">
-                  Gender <code>*</code>
+                <CFormLabel className="mt-2">
+                  Qualification <code>*</code>
                 </CFormLabel>
                 <CFormInput
-                  value={gender}
+                  value={qualification}
                   onChange={(e) => {
-                    setGender(e.target.value);
+                    setqualification(e.target.value);
+                    QualificationValidation(e);
                   }}
-                  disabled={!fname || !lname || !date || !Faname || !mname}
-                  type=""
-                  placeholder="1-100"
+                  // options={options}
+                  disabled={
+                    !fname ||
+                    !lname ||
+                    !date ||
+                    !Faname ||
+                    !mname ||
+                    FanameError ||
+                    mnameError ||
+                    fnameError
+                  }
                 />
+                <span className="errorMsg">{qualificationError}</span>
               </CCol>
             </CRow>
-            <CFormLabel className=" mt-2">
-              Email <code>*</code>
-            </CFormLabel>
-            <CFormInput
-              value={mail}
-              onChange={(e) => {
-                setMail(e.target.value);
-              }}
-              disabled={
-                !fname || !lname || !Faname || !mname || !date || !gender
-              }
-              type="email"
-              placeholder="name@example.com"
-            />
-            <CFormLabel className=" mt-2">
-              Pnone Number <code>*</code>
-            </CFormLabel>
-            <CFormInput
-              value={phone}
-              onChange={(e) => {
-                setPhone(e.target.value);
-              }}
-              disabled={
-                !fname ||
-                !lname ||
-                !Faname ||
-                !mname ||
-                !date ||
-                !mail ||
-                !gender
-              }
-              type="text"
-              placeholder="9999998888"
-            />
+            <CRow>
+              <CCol sm={6}>
+                <CFormLabel className=" mt-2">
+                  Email <code>*</code>
+                </CFormLabel>
+                <CFormInput
+                  value={mail}
+                  onChange={(e) => {
+                    setMail(e.target.value);
+                    emailValidation(e);
+                  }}
+                  disabled={
+                    !fname ||
+                    !lname ||
+                    !Faname ||
+                    !mname ||
+                    !date ||
+                    !qualification ||
+                    FanameError ||
+                    mnameError ||
+                    fnameError
+                  }
+                  type="email"
+                  placeholder="name@example.com"
+                />
+                <span className="errorMsg">{mailError}</span>
+              </CCol>
+              <CCol sm={6}>
+                <CFormLabel className=" mt-2">
+                  Pnone Number <code>*</code>
+                </CFormLabel>
+                <CFormInput
+                  value={phone}
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                    PhoneNumberValidation(e);
+                  }}
+                  disabled={
+                    !fname ||
+                    !lname ||
+                    !Faname ||
+                    !mname ||
+                    !date ||
+                    !mail ||
+                    !qualification ||
+                    FanameError ||
+                    mnameError ||
+                    fnameError ||
+                    mailError
+                  }
+                  type="text"
+                  placeholder="9999998888"
+                />
+                <span className="errorMsg">{phoneError}</span>
+              </CCol>
+            </CRow>
 
             <CButton
-              onClick={() => createFuntion()}
+              onClick={() => SaveDetails()}
               className="mt-4 mx-2"
               color="success"
               variant="outline"
@@ -256,7 +527,12 @@ function Details() {
                 !date ||
                 !mail ||
                 !phone ||
-                !gender
+                !qualification ||
+                FanameError ||
+                mnameError ||
+                fnameError ||
+                mailError ||
+                phoneError
               }
             >
               Save
@@ -272,26 +548,37 @@ function Details() {
           </CForm>
         </CCol>
         <CCol sm={8}>
-          <div className="tableWidth m-5">
+          <div className="tableWidth">
             <CSmartTable
               itemsPerPageSelect
               clickableRows
               columns={columns}
               //   columns={columns.filter((x) => x.label != "Action")}
-              items={data}
-              // itemsPerPage={Number(itemsPerPage)}
+              items={getData}
+              itemsPerPage={5}
+              pagination
               scopedColumns={{
                 show_details: (item) => {
                   return (
                     <div>
                       <td className="gaponly">
-                        <button className="updateBtn" onClick={() => {}}>
+                        <button
+                          className="updateBtn"
+                          onClick={() => {
+                            DetailsUpdate(item.DetailsID);
+                          }}
+                        >
                           <FaEdit
                             style={{ fontSize: "20px", color: "#002663" }}
                           />
                         </button>
-
-                        <button className="deleteBtn" onClick={() => {}}>
+                        {/* {JSON.stringify(item)} */}
+                        <button
+                          className="deleteBtn"
+                          onClick={() => {
+                            DeleteTask(item.DetailsID);
+                          }}
+                        >
                           <RiDeleteBinFill
                             style={{ fontSize: "22px", color: "#ea4335" }}
                           />
@@ -300,6 +587,11 @@ function Details() {
                     </div>
                   );
                 },
+                //   MotherName:(items)=>{
+                //     return(
+                //       {FatherName }
+                //     )
+                //   }
               }}
               sorterValue={{ column: "name", state: "asc" }}
               tableProps={{
